08ebcf4fc30b1901119ae1ab9398e70f32fef42a
BREEXZED <breexzed@outlook.com>
2026-09-15 23:19:09 +0100

    implemented stage 2 logging. Logged the ip adrr and duration of transacction for requests hitting the server. learnt that i still have some things to fix like multiplexing& routing, keep alive persistence etc. cleaned up the codebase and made it somewhat more readable, added code-comments on thing's i've learnt and also used a struct to hold the request info, just for readability purposes.

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -22,6 +23,9 @@ func main() {
 		}
 		fmt.Println("Accepted new connection")
 
+		//wrapped handleConnection in a goroutine this specific way
+		//so I can handle errors for any connection that fails without it affecting the others
+		//initially was go handleConnection(conn)
 		go func() {
 			err := handleConnection(conn)
 			if err != nil {
@@ -32,15 +36,34 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) error {
+
+	//used a struct cause I like reading elegant code, not really necessary
+	//probably not the right way to do this but going to let this one slide
+	type Request struct {
+		method   string
+		target   string
+		addr     string
+		version  string
+		duration int
+	}
+
+	//ensure connections closes after it's being handles
+	//finally i get to use this stuff in the real world. very useful.
 	defer conn.Close()
 
+	start := time.Now()
+	ipAddr := conn.RemoteAddr().String()
+
+	//without bufio I wouldn't have been able to read off conn(which is a byte)
+	//Off is the word here cause this implies I'm reading its content on the fly
+	//which is exactly what I went on t do with it
 	reader := bufio.NewReader(conn)
 
 	line, err := reader.ReadString('\n')
 	if err != nil {
 		return err
 	}
-	if !strings.HasSuffix(line, "\r\n") {
+	if !strings.HasSuffix(line, "\r\n") { //impressive strings.Method from the golang engineers
 
 		return fmt.Errorf("malformed line, missing CRLF: %q", line)
 	}
@@ -50,9 +73,29 @@ func handleConnection(conn net.Conn) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("parsed request: method=%s target=%s version=%s\n", method, target, version)
 
-	response := "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK"
+	r := Request{
+		method:   method,
+		target:   target,
+		addr:     ipAddr,
+		version:  version,
+		duration: int(time.Since(start).Milliseconds()),
+	}
+	fmt.Printf("parsed request:\n"+
+		"method: %s\n"+
+		"target: %s\n"+
+		"addr: %s\n"+
+		"version: %s\n"+
+		"duration: %d\n",
+		r.method, r.target, r.addr, r.version, r.duration)
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Length: 2\r\n\r\n" +
+		"OK"
+
+	//It was not clear to me how this even is possible but basically what it does is
+	//it asserts that the only value that matters here is error, so we deal with it here,
+	//as well as in this function usage back in main wrapped in a goroutine
 	_, err = conn.Write([]byte(response))
 	if err != nil {
 		return err
@@ -62,6 +105,9 @@ func handleConnection(conn net.Conn) error {
 }
 
 func parseRequestLine(line string) (method, target, version string, err error) {
+
+	//learnt a ton about the string package writing this function
+	//not so much but enough that I can now think about how to use it
 	parts := strings.Split(line, " ")
 	if len(parts) != 3 {
 		return "", "", "", fmt.Errorf("malformed line: %q", line)
